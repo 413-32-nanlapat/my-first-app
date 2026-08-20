d2b4af585ba1e1cb3ee4206f0d7f4d41c47ba0ba
413-32-nanlapat <56869@yupparaj.ac.th>
2026-08-20 09:30:44 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -101,4 +101,4 @@ if st.session_state.get("is_ended", False):
     show_result_dialog(ans1, ans2)
 
 st.divider()
-  st.write("นางสาวนันท์ลภัส จำต๊ะ เลขที่ 31 ม.4/13")
+st.write("นางสาวนันท์ลภัส จำต๊ะ เลขที่ 30 ม.4/13")
