e656d1bb0b509ab1a6f32fa91d6d13a852a8d37f
413-32-nanlapat <56869@yupparaj.ac.th>
2026-09-10 09:54:28 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -116,9 +116,9 @@ st.session_state.ans4_val = ans4
  
 # 4. ปุ่มส่งคำตอบ
 if "start" in st.session_state and not st.session_state.get("is_ended", False):
-if st.button("📥 ส่งคำตอบ"):
-st.session_state.is_ended = True
-st.rerun()
+    if st.button("📥 ส่งคำตอบ"):
+        st.session_state.is_ended = True
+        st.rerun()
  
 time.sleep(1)
 st.rerun()
