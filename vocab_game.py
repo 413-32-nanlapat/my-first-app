8c037a04e50b1c6eef54d05dae4e92a8e96fd310
413-32-nanlapat <56869@yupparaj.ac.th>
2026-09-10 09:52:14 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -83,8 +83,8 @@ if "start" in st.session_state and not st.session_state.get("is_ended", False):
     time_left = int(30 - (time.time() - st.session_state.start))
  
     if time_left > 0:
- st.error(f"⏳ เหลือเวลา: {time_left} วินาที")
-   else:
+       st.error(f"⏳ เหลือเวลา: {time_left} วินาที")
+    else:
         st.session_state.is_ended = True
         st.rerun()
  
