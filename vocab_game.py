46dbfbfe0f9392d95c459fac5899bd1a840541c7
413-32-nanlapat <56869@yupparaj.ac.th>
2026-09-10 09:48:51 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -29,48 +29,48 @@ def reset_game():
 # ----------------------------------------------------
 @st.dialog("📊 สรุปผลการเล่นเกม")
 def show_result_dialog(ans1, ans2, ans3, ans4):
-st.balloons()
-score = 0
+ st.balloons()
+ score = 0
  
-u_ans1 = ans1.strip().lower()
-u_ans2 = ans2.strip().lower()
-u_ans3 = ans3.strip().lower()
-u_ans4 = ans4.strip().lower()
+ u_ans1 = ans1.strip().lower()
+ u_ans2 = ans2.strip().lower()
+ u_ans3 = ans3.strip().lower()
+ u_ans4 = ans4.strip().lower()
  
 # ตรวจข้อ 1
 if u_ans1 == "apple":
-st.success("✅ ข้อ 1: ถูกต้อง")
-score += 1
+ st.success("✅ ข้อ 1: ถูกต้อง")
+ score += 1
 else:
-st.error(f"❌ ข้อ 1: ยังไม่ถูกต้อง (คุณตอบ '{u_ans1}')")
+     st.error(f"❌ ข้อ 1: ยังไม่ถูกต้อง (คุณตอบ '{u_ans1}')")
  
 # ตรวจข้อ 2
 if u_ans2 == "fish":
-st.success("✅ ข้อ 2: ถูกต้อง")
-score += 1
+ st.success("✅ ข้อ 2: ถูกต้อง")
+ score += 1
 else:
-st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
+     st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
  
 # ตรวจข้อ 3
 if u_ans3 == "banana":
-st.success("✅ ข้อ 3: ถูกต้อง")
-score += 1
+ st.success("✅ ข้อ 3: ถูกต้อง")
+ score += 1
 else:
-st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
+     st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
  
 # ตรวจข้อ 4
 if u_ans4 == "carrot":
-st.success("✅ ข้อ 4: ถูกต้อง")
-score += 1
+ st.success("✅ ข้อ 4: ถูกต้อง")
+ score += 1
 else:
-st.error(f"❌ ข้อ 4: ยังไม่ถูกต้อง (คุณตอบ '{u_ans4}')")
+     st.error(f"❌ ข้อ 4: ยังไม่ถูกต้อง (คุณตอบ '{u_ans4}')")
  
 st.info(f"🏆 ได้คะแนนรวม: {score} คะแนน")
  
 if score == 4:
-st.success("🎉 You win!")
+ st.success("🎉 You win!")
 else:
-st.error("💀 You lose!")
+ st.error("💀 You lose!")
  
  
 # ----------------------------------------------------
@@ -83,10 +83,10 @@ if "start" in st.session_state and not st.session_state.get("is_ended", False):
 time_left = int(30 - (time.time() - st.session_state.start))
  
 if time_left > 0:
-st.error(f"⏳ เหลือเวลา: {time_left} วินาที")
+ st.error(f"⏳ เหลือเวลา: {time_left} วินาที")
 else:
-st.session_state.is_ended = True
-st.rerun()
+ st.session_state.is_ended = True
+ st.rerun()
  
 st.divider()
  
